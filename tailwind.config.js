9c19842491c8519cd382f2aa720ef9e547751194
Robert Laws <roblaws@me.com>
2021-04-14 10:46:38 +0300

    add tailwindcss support

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+const colors = require('tailwindcss/colors');
+
+module.exports = {
+  purge: {
+    mode: 'layers',
+    content: [
+      'src/**/*.js',
+      'src/**/*.jsx',
+      'src/**/*.ts',
+      'src/**/*.tsx',
+      'public/**/*.html',
+    ],
+  },
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        gray: colors.trueGray,
+        emerald: colors.emerald,
+        teal: colors.teal,
+      },
+    },
+  },
+  variants: {
+    extend: {
+      backgroundColor: ['active'],
+    },
+  },
+  plugins: [require('@tailwindcss/forms')],
+};
