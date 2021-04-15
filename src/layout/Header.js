b0fd7d769e50df6ad332a234b57588d23aa8856a
Robert Laws <roblaws@me.com>
2021-04-15 10:25:02 +0300

    add header and navigation styling

--- src/layout/Header.js
+++ src/layout/Header.js
@@ -1,12 +1,21 @@
 import React from 'react';
+import { ReactComponent as Logo } from '../assets/images/logo-3.svg';
 import Navigation from './Navigation';
 
 const Header = () => {
   return (
-    <div>
-      Header
-      <Navigation />
-    </div>
+    <header className='text-gray-700 bg-blue-700 body-font'>
+      <div className='container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center'>
+        <a className='flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0'>
+          <Logo className='w-12 h-12 p-2 bg-blue-400 rounded-full' />
+          <span className='ml-3 text-lg text-white'>LessonBuilder</span>
+        </a>
+        <Navigation />
+        <button className='inline-flex items-center bg-gray-200 border-0 py-2 px-4 focus:outline-none hover:bg-blue-500 hover:text-white rounded text-base mt-4 md:mt-0'>
+          Login
+        </button>
+      </div>
+    </header>
   );
 };
 
--- src/layout/Navigation.js
+++ src/layout/Navigation.js
@@ -1,7 +1,19 @@
 import React from 'react';
 
 const Navigation = () => {
-  return <div>Navigation</div>;
+  return (
+    <nav className='md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-100	flex flex-wrap items-center text-base justify-center'>
+      <a className='mr-5 text-white hover:text-blue-100 cursor-pointer'>
+        Lessons List
+      </a>
+      <a className='mr-5 text-white hover:text-blue-100 cursor-pointer'>
+        New Lesson
+      </a>
+      <a className='mr-5 text-white hover:text-blue-100 cursor-pointer'>
+        About
+      </a>
+    </nav>
+  );
 };
 
 export default Navigation;
