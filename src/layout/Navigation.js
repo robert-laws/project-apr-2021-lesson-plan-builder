6d7768b660bda171d2b18fe0fdb455ad26d7e682
Robert Laws <roblaws@me.com>
2021-04-17 15:50:50 +0300

    conditional rendering of nav links for authenticated user

--- src/layout/Navigation.js
+++ src/layout/Navigation.js
@@ -1,21 +1,29 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { NavLink } from 'react-router-dom';
+import AuthenticationContext from '../context/authentication/authenticationContext';
 
 const Navigation = () => {
+  const authenticationContext = useContext(AuthenticationContext);
+  const { isAuthenticated } = authenticationContext;
+
   return (
     <nav className='md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-100	flex flex-wrap items-center text-base justify-center'>
-      <NavLink
-        to='/lists'
-        className='mr-5 text-white hover:text-blue-100 cursor-pointer'
-      >
-        Lessons List
-      </NavLink>
-      <NavLink
-        to='/new'
-        className='mr-5 text-white hover:text-blue-100 cursor-pointer'
-      >
-        New Lesson
-      </NavLink>
+      {isAuthenticated && (
+        <NavLink
+          to='/lists'
+          className='mr-5 text-white hover:text-blue-100 cursor-pointer'
+        >
+          Lessons List
+        </NavLink>
+      )}
+      {isAuthenticated && (
+        <NavLink
+          to='/new'
+          className='mr-5 text-white hover:text-blue-100 cursor-pointer'
+        >
+          New Lesson
+        </NavLink>
+      )}
       <NavLink
         to='/about'
         className='mr-5 text-white hover:text-blue-100 cursor-pointer'
