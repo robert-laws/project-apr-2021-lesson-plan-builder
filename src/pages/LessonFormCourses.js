b08cb525e73306ce6a719f00da77d0943623fccd
Robert Laws <roblaws@me.com>
2021-04-19 11:59:40 +0300

    adjust spacing for form buttons

--- src/pages/LessonFormCourses.js
+++ src/pages/LessonFormCourses.js
@@ -1,15 +1,17 @@
 import React from 'react';
+import Section from '../layout/Section';
 import Heading from '../components/ui/Heading';
 import Button from '../components/ui/Button';
 
 const LessonFormCourses = ({ handleAdvanceStep }) => {
   return (
-    <div>
+    <Section>
       <Heading size='h2'>Course Details</Heading>
-      <div className='flex justify-end'>
+      <Section></Section>
+      <div className='flex justify-end mt-4'>
         <Button handleClick={handleAdvanceStep} buttonText='Next Step' />
       </div>
-    </div>
+    </Section>
   );
 };
 
