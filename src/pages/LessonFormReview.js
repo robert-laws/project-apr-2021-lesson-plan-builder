c95b1f3ecf12a884649786fa408eddf25b0dd9f2
Robert Laws <roblaws@me.com>
2021-04-21 18:14:40 +0300

    add buttonspinner component for forms

--- src/pages/LessonFormReview.js
+++ src/pages/LessonFormReview.js
@@ -2,6 +2,7 @@ import React, { useContext, useState, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
 import Heading from '../components/ui/Heading';
 import Button from '../components/ui/Button';
+import ButtonSpinner from '../components/ui/ButtonSpinner';
 import AuthenticationContext from '../context/authentication/authenticationContext';
 import OptionsContext from '../context/options/optionsContext';
 import LessonsContext from '../context/lessons/lessonsContext';
@@ -264,28 +265,7 @@ const LessonFormReview = ({ handleReverseStep }) => {
           type='button'
           className='inline-flex items-center justify-center px-5 py-2 border border-transparent text-base font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 disabled:opacity-50 cursor-pointer disabled:cursor-default disabled:bg-gray-400'
         >
-          {savingLesson && (
-            <svg
-              className='animate-spin -ml-1 mr-3 h-5 w-5 text-white'
-              xmlns='http://www.w3.org/2000/svg'
-              fill='none'
-              viewBox='0 0 24 24'
-            >
-              <circle
-                className='opacity-25'
-                cx='12'
-                cy='12'
-                r='10'
-                stroke='currentColor'
-                strokeWidth='4'
-              ></circle>
-              <path
-                className='opacity-75'
-                fill='currentColor'
-                d='M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z'
-              ></path>
-            </svg>
-          )}
+          {savingLesson && <ButtonSpinner />}
           Save Lesson
         </button>
       </div>
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect, useCallback, useContext } from 'react';
 import { useHistory } from 'react-router-dom';
+import ButtonSpinner from '../components/ui/ButtonSpinner';
 import TextInput from '../components/ui/TextInput';
 import PasswordInput from '../components/ui/PasswordInput';
 import AuthenticationContext from '../context/authentication/authenticationContext';
@@ -114,28 +115,7 @@ const Login = () => {
                   type='submit'
                   className='w-full inline-flex justify-center items-center mx-auto text-white bg-indigo-500 hover:bg-indigo-600 border-0 py-2 px-8 focus:outline-none rounded text-lg cursor-pointer'
                 >
-                  {isUserLoading && (
-                    <svg
-                      className='animate-spin -ml-1 mr-3 h-5 w-5 text-white'
-                      xmlns='http://www.w3.org/2000/svg'
-                      fill='none'
-                      viewBox='0 0 24 24'
-                    >
-                      <circle
-                        className='opacity-25'
-                        cx='12'
-                        cy='12'
-                        r='10'
-                        stroke='currentColor'
-                        strokeWidth='4'
-                      ></circle>
-                      <path
-                        className='opacity-75'
-                        fill='currentColor'
-                        d='M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z'
-                      ></path>
-                    </svg>
-                  )}
+                  {isUserLoading && <ButtonSpinner />}
                   Login
                 </button>
               </div>
