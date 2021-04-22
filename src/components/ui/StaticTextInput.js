5d017082122c145b80ad1e6ecd2a3a72e3fe69ae
Robert Laws <roblaws@me.com>
2021-04-22 19:15:49 +0300

    format static text input components

--- src/components/ui/StaticTextInput.js
+++ src/components/ui/StaticTextInput.js
@@ -26,7 +26,7 @@ const StaticTextInput = ({
         <Label>{labelName}</Label>
         <input
           type='text'
-          className='mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50'
+          className='mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-100 focus:ring focus:ring-indigo-50 focus:ring-opacity-5 bg-gray-100 text-gray-700'
           placeholder=''
           name={inputName}
           id={inputName}
