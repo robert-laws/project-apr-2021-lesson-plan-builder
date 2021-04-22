24843dbe81c552acdb4882b61cb3e2db63277c7f
Robert Laws <roblaws@me.com>
2021-04-22 19:02:06 +0300

    add content to about and home page features

--- src/layout/Features.js
+++ src/layout/Features.js
@@ -12,7 +12,12 @@ const Features = () => {
           <h4 className='text-3xl text-gray-800 font-bold mb-3'>
             Lesson Builder
           </h4>
-          <p className='text-gray-600 mb-8'>Build a lesson.</p>
+          <p className='text-gray-600 mb-8'>
+            Quickly build new lessons by using the database-driven form that
+            steps through the lesson creation process. Learning modules can be
+            created and organized by using a drag-and-drop feature to move them
+            into different sequence of order.
+          </p>
         </div>
         <div className='w-full md:w-1/2'>
           <img src={placeholderImage} alt='Monitoring' />
@@ -27,7 +32,11 @@ const Features = () => {
           <h4 className='text-3xl text-gray-800 font-bold mb-3'>
             Lessons List
           </h4>
-          <p className='text-gray-600 mb-8'>View a list of all lessons.</p>
+          <p className='text-gray-600 mb-8'>
+            View a list of all lessons that have been built and saved to the
+            database. The list of lessons includes brief detailed about each
+            lesson and a link to view more lesson details.
+          </p>
         </div>
       </div>
 
@@ -37,7 +46,9 @@ const Features = () => {
             Lesson Detail
           </h4>
           <p className='text-gray-600 mb-8'>
-            View details of an individual lesson.
+            View details of an individual lesson. Each lesson includes extensive
+            details about the lesson. Everything is pulled from the databases on
+            demand. The lesson details are formatted for improved readability.
           </p>
         </div>
         <div className='w-full md:w-1/2'>
--- src/pages/About.js
+++ src/pages/About.js
@@ -10,9 +10,34 @@ const About = () => {
       <section className='container mx-auto px-6 p-10'>
         <div className='flex flex-wrap mb-20 items-start'>
           <div className='w-full md:w-3/4 md:pr-10'>
-            <p className='text-gray-600 mb-8'>
-              The LessonBuilder website is designed to help instructors create
-              lesson plans quickly.
+            <Heading size='h3'>Logging into the Website</Heading>
+            <p className='text-gray-600 mb-8 mt-2'>
+              The lesson building and list components of the website are
+              accessible only to logged in users of the site. Users who are
+              logged in are authorized to view and create new lessons.
+            </p>
+            <Heading size='h3'>Creating Lessons</Heading>
+            <p className='text-gray-600 mb-8 mt-2'>
+              The LessonBuilder website is designed to help library and
+              information literacy instructors create lesson plans quickly. The
+              Lesson building process includes four steps. Each lesson is tied
+              to a course in the University curriculum. Step one allows the user
+              to select a course from the curriculum and auto-populates the form
+              fields with data. The next step is add session details. This step
+              captures information like the session date, number of learners,
+              information literacy objectives, threshold concepts, resources
+              used, and more. The third step is add individual instructional
+              modules with detailed information about the order for each module,
+              specific steps for each module, and the time for each module. The
+              last step is to review the lesson and save it to the database.
+            </p>
+            <Heading size='h3'>Lesson List and Details</Heading>
+            <p className='text-gray-600 mb-8 mt-2'>
+              Once a new lesson has been saved it can be viewed within the list
+              of all lessons. The list provides a condensed view of the lesson
+              details. Each lesson can be viewed in greater detail. Lessons can
+              be retrieved for reference later when a class instruction is being
+              delivered.
             </p>
           </div>
           <div className='w-full md:w-1/4'>
