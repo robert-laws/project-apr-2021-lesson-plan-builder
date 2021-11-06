ed16a8b3f174afb3778a1292732ed5a4ce915d90
Robert Laws <roblaws@me.com>
2021-11-06 11:12:54 +0300

    add dynamic button text for hero button

--- src/layout/Footer.js
+++ src/layout/Footer.js
@@ -9,7 +9,6 @@ const Footer = () => {
       <div className='bg-gray-100'>
         <div className='container px-5 py-6 mx-auto flex items-center sm:flex-row flex-col h-32'>
           <Link
-            exact
             to='/'
             className='flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0'
           >
--- src/layout/Hero.js
+++ src/layout/Hero.js
@@ -1,7 +1,11 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
+import AuthenticationContext from '../context/authentication/authenticationContext';
 
 const Hero = () => {
+  const authenticationContext = useContext(AuthenticationContext);
+  const { isAuthenticated } = authenticationContext;
+
   return (
     <section className='py-20 hero-section'>
       <div className='container mx-auto px-6'>
@@ -12,12 +16,21 @@ const Hero = () => {
           Speed up the process with data automation
         </h3>
 
-        <Link
-          to='/login'
-          className='bg-white font-bold rounded-full py-4 px-8 shadow-lg uppercase tracking-wider'
-        >
-          Login to Start
-        </Link>
+        {isAuthenticated ? (
+          <Link
+            to='/new'
+            className='bg-white font-bold rounded-full py-4 px-8 shadow-lg uppercase tracking-wider'
+          >
+            Build a New Lesson
+          </Link>
+        ) : (
+          <Link
+            to='/login'
+            className='bg-white font-bold rounded-full py-4 px-8 shadow-lg uppercase tracking-wider'
+          >
+            Login to Start
+          </Link>
+        )}
       </div>
     </section>
   );
