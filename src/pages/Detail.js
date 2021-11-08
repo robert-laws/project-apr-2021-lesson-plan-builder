8b587381a6d2a31152370125cb921dd86b27b1b3
Robert Laws <roblaws@me.com>
2021-11-08 06:18:33 +0300

    add return to lists and delete button on list detail

--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -1,9 +1,13 @@
-import React, { useEffect, useContext } from 'react';
+import React, { useEffect, useContext, useState } from 'react';
+import { useHistory, Link } from 'react-router-dom';
 import Section from '../layout/Section';
+import DeleteButton from '../components/ui/DeleteButton';
+import ButtonSpinner from '../components/ui/ButtonSpinner';
 import Heading from '../components/ui/Heading';
 import Spinner from '../components/ui/Spinner';
 import LessonCard from '../components/ui/LessonCard';
 import LessonDetail from '../components/ui/LessonDetail';
+import AuthenticationContext from '../context/authentication/authenticationContext';
 import LessonsContext from '../context/lessons/lessonsContext';
 
 const Detail = ({
@@ -12,12 +16,26 @@ const Detail = ({
   thresholdConcepts,
   informationLiteracyObjectives,
 }) => {
+  const authenticationContext = useContext(AuthenticationContext);
+  const { cookie } = authenticationContext;
+
   const lessonsContext = useContext(LessonsContext);
-  const { lesson, getLesson } = lessonsContext;
+  const { lesson, getLesson, deleteLesson } = lessonsContext;
+
+  const history = useHistory();
 
   useEffect(() => {
     getLesson(lessonId);
   }, [getLesson, lessonId]);
+
+  const [deletingLesson, setDeletingLesson] = useState('');
+
+  const handleDeleteClick = async (e) => {
+    setDeletingLesson(e.target.name);
+    await deleteLesson(e.target.name, cookie);
+    setDeletingLesson('');
+    history.push(`/lists`);
+  };
 
   return (
     <Section>
@@ -54,6 +72,21 @@ const Detail = ({
           />
         </LessonCard>
       )}
+      <div className='flex justify-between align-bottom m-4'>
+        <Link
+          to='/lists'
+          className='inline-flex items-center justify-center px-5 py-2 border border-transparent text-base font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 disabled:opacity-50 cursor-pointer disabled:cursor-default disabled:bg-gray-400'
+        >
+          Return to All Lessons
+        </Link>
+        <DeleteButton
+          handleClick={handleDeleteClick}
+          id={lesson.id}
+          buttonText='Delete this Lesson'
+        >
+          {deletingLesson === lesson.id.toString() && <ButtonSpinner />}
+        </DeleteButton>
+      </div>
     </Section>
   );
 };
